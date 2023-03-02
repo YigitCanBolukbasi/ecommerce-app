12f36bbfa30d2e872ac78f6977ccff1e9faf0a57
Yiğit Can bölükbaşı <91588692+YigitCanBolukbasi@users.noreply.github.com>
2023-03-03 01:20:55 +0300

    footer searchbar is fixed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -21,41 +21,14 @@ const data = [
 ];
 
 const Search = styled("div")(({ theme }) => ({
-  color: "black",
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: "white",
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(3),
-    width: "auto",
-  },
+  background: "white",
+  width: "80%",
 }));
 
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
+const SearchIconWrapper = styled("div")(({ theme }) => ({}));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("md")]: {
-      width: "20ch",
-    },
-  },
+  background: "white",
 }));
 
 const Container = styled(Box)(({ theme }) => ({
@@ -93,14 +66,18 @@ function Footer() {
             </Grid>
             <Grid xs={8}>
               <Search>
-                <SearchIconWrapper>
-                  <SearchIcon />
-                </SearchIconWrapper>
-                <StyledInputBase
-                  placeholder="Search…"
-                  inputProps={{ "aria-label": "search" }}
-                />
-                <StyledButton>Sign Up</StyledButton>
+                <Stack flexDirection="row" justifyContent="flex-end">
+                  <Box>
+                    <SearchIconWrapper>
+                      <SearchIcon />
+                    </SearchIconWrapper>
+                  </Box>
+                  <StyledInputBase
+                    placeholder="Search…"
+                    inputProps={{ "aria-label": "search" }}
+                  />
+                  <StyledButton>Sign Up</StyledButton>
+                </Stack>
               </Search>
             </Grid>
           </Grid>
